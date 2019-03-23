ba7494c6761957f34a6af36c1a3175a00efd1843
jheng.wei0919@gmail.com <JackPan@jackpan-MacBook-Pro.local>
2019-03-24 00:14:30 +0800

    fix checkPermission  and  map can move now location

--- app/src/main/java/com/student/student_searchmap/LandingPageActivity.kt
+++ app/src/main/java/com/student/student_searchmap/LandingPageActivity.kt
@@ -1,26 +1,51 @@
 package com.student.student_searchmap
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.RelativeLayout
 import android.widget.TextView
-
-import kotlinx.android.synthetic.main.activity_landing_page.*
+import android.widget.Toast
 import java.util.*
 
-class LandingPageActivity : AppCompatActivity() {
+
+class LandingPageActivity : Activity() {
     lateinit var  mTextView :TextView
+    lateinit var    mRelativeLayout:RelativeLayout
     var mChange :Boolean = false
+    private lateinit var managePermissions: ManagePermissions
+
+    private val MY_PERMISSIONS_REQUEST_LOCATION = 1
+    val list = listOf<String>(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
+
+        checkPermission()
+
         initLayout()
         changeColor()
+
     }
     fun  initLayout(){
         mTextView  = findViewById(R.id.text)
-
+        mRelativeLayout = findViewById(R.id.layout)
+        mRelativeLayout.setOnClickListener {
+            startActivity(Intent(this,MyMapActivity::class.java))
+        }
     }
     fun changeColor(){
         val timer = Timer()
@@ -41,6 +66,25 @@ class LandingPageActivity : AppCompatActivity() {
             }
         }
     }
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_LOCATION)
+        }
 
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
 
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(this, "需要定位功能,才能使用喔", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+    }
+
+    fun Context.toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/student/student_searchmap/MyMapActivity.kt
+++ app/src/main/java/com/student/student_searchmap/MyMapActivity.kt
@@ -1,7 +1,16 @@
 package com.student.student_searchmap
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,17 +19,49 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MyMapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MyMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraMoveCanceledListener
+        , LocationListener, GoogleMap.OnCameraMoveStartedListener,GoogleMap.OnCameraIdleListener {
+    override fun onCameraMove() {
+    }
+
+    override fun onCameraMoveCanceled() {
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+    }
+
+    override fun onCameraMoveStarted(p0: Int) {
+    }
+
+    override fun onCameraIdle() {
+    }
 
     private lateinit var mMap: GoogleMap
+    private var locationManager: LocationManager? = null
+    val MY_PERMISSIONS_REQUEST_LOCATION = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_map)
+        checkPermission()
+        initLayout()
+    }
+
+    @SuppressLint("ServiceCast")
+    fun initLayout() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        try {
+            // Request location updates
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        } catch (ex: SecurityException) {
+            Log.d("myTag", "Security Exception, no location available")
+        }
+
     }
 
     /**
@@ -32,12 +73,56 @@ class MyMapActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setMyLocationEnabled(true)
+        mMap.setOnCameraMoveStartedListener(this)
+        mMap.setOnCameraMoveListener(this)
+        mMap.setOnCameraMoveCanceledListener(this)
+        mMap.setOnCameraIdleListener(this)
+    }
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+        }
+
+        override fun onLocationChanged(location: Location) {
+            Log.d("Location", location.latitude.toString())
+            Log.d("Location", location.longitude.toString())
+            var latlon: String = location.latitude.toString() + "," + location.longitude.toString()
+//            locationTextView.text = "${location.latitude} - ${location.longitude}"
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 18.0f))
+
+            }
+
+
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(this, "需要定位功能,才能使用喔", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+    }
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_LOCATION)
+        }
+
     }
 }

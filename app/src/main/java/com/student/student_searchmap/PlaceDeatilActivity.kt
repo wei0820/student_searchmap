dc4e58610075e335276b68be5a186de6b3bd8d9c
wei0820 <jheng.wei0919@gmail.com>
2019-04-01 14:43:47 +0800

    add view

--- app/src/main/java/com/student/student_searchmap/PlaceDeatilActivity.kt
+++ app/src/main/java/com/student/student_searchmap/PlaceDeatilActivity.kt
@@ -3,6 +3,11 @@ package com.student.student_searchmap
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import com.hendraanggrian.pikasso.picasso
 import com.student.student_searchmap.Data.GoogleMapPlaceDetailsData
 import com.student.student_searchmap.Data.GoogleResponseData
 
@@ -22,6 +27,7 @@ class PlaceDeatilActivity : AppCompatActivity(), GoogleMapAPISerive.GetResponse 
             }
         }
     }
+    lateinit var mReViewListView: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +45,24 @@ class PlaceDeatilActivity : AppCompatActivity(), GoogleMapAPISerive.GetResponse 
         Log.d("PlaceDeatilActivity",GoogleMapAPISerive.getPhotos(this,url))
 
 //        GoogleMapAPISerive.getPlaceDeatail(this, id, this)
+
+    }
+    fun addnewLayout(data: GoogleMapPlaceDetailsData.Result.Reviews) {
+        val view = getLayoutInflater().inflate(R.layout.review_layout, null)
+        var mReviewImg: ImageView = view!!.findViewById(R.id.reviewimg)
+        var mReviewName: TextView = view!!.findViewById(R.id.reviewname)
+        var mReviewText: TextView = view!!.findViewById(R.id.reviewtext)
+        var mReviewTime: TextView = view!!.findViewById(R.id.reviewtimetext)
+        var mReviewRating: RatingBar = view!!.findViewById(R.id.rating)
+
+        if (data.profile_photo_url != null) {
+            picasso.load(data.profile_photo_url).error(R.mipmap.ic_launcher).into(mReviewImg)
+        }
+        mReviewName.text = data.author_name
+        mReviewText.text = data.text
+        mReviewRating.rating = data.rating
+        mReViewListView.addView(view)
+
 
     }
 

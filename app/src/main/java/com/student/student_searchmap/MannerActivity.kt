d1d7dcc6428a8ee40449f3d280b10f1c60c8b5e5
wei0820 <jheng.wei0919@gmail.com>
2019-07-28 18:19:19 +0800

    add function

--- app/src/main/java/com/student/student_searchmap/MannerActivity.kt
+++ app/src/main/java/com/student/student_searchmap/MannerActivity.kt
@@ -29,30 +29,25 @@ import java.util.*
 import android.util.Base64
 
 
-class MannerActivity : AppCompatActivity(),View.OnClickListener {
+class MannerActivity : AppCompatActivity(), View.OnClickListener {
 
     private val CAMERA = 66
     private val PHOTO = 99
     private val REQUEST_EXTERNAL_STORAGE = 200
     private val PICKER = 100
-
     lateinit var bitmap: Bitmap
     lateinit var phone: DisplayMetrics
-
     var img: String = ""
-
-
-
-    lateinit var mAddressEdt :EditText
-    lateinit var mCheckBtn :Button
-    lateinit var mSpinner :Spinner
-    lateinit var mStartbtn :Button
-    lateinit var mEndbtn :Button
-    lateinit var mPriceEdt :EditText
-    lateinit var mPhotoButtton :Button
-    lateinit var mPhoneEdt :EditText
-    lateinit var mMessageEdt :EditText
-    lateinit var mSendBtn :Button
+    lateinit var mAddressEdt: EditText
+    lateinit var mCheckBtn: Button
+    lateinit var mSpinner: Spinner
+    lateinit var mStartbtn: Button
+    lateinit var mEndbtn: Button
+    lateinit var mPriceEdt: EditText
+    lateinit var mPhotoButtton: Button
+    lateinit var mPhoneEdt: EditText
+    lateinit var mMessageEdt: EditText
+    lateinit var mSendBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manner)
@@ -61,7 +56,8 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
 
         initLayout()
     }
-    fun initLayout(){
+
+    fun initLayout() {
         mAddressEdt = findViewById(R.id.editText)
         mCheckBtn = findViewById(R.id.checkbtn)
         mStartbtn = findViewById(R.id.startbtn)
@@ -74,8 +70,7 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
         mSendBtn = findViewById(R.id.send)
 
 
-
-        val searchSortSpinnerData = arrayOf("室內","室外")
+        val searchSortSpinnerData = arrayOf("室內", "室外")
         val adapter = ArrayAdapter(
                 this, // Context
                 android.R.layout.simple_spinner_item, // Layout
@@ -87,12 +82,12 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
         mSpinner.adapter = adapter;
 
         // Set an on item selected listener for spinner object
-        mSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent:AdapterView<*>, view: View, position: Int, id: Long){
-                Toast.makeText(this@MannerActivity,"選擇"+searchSortSpinnerData[position],Toast.LENGTH_SHORT).show()
+        mSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                Toast.makeText(this@MannerActivity, "選擇" + searchSortSpinnerData[position], Toast.LENGTH_SHORT).show()
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>){
+            override fun onNothingSelected(parent: AdapterView<*>) {
                 // Another interface callback
             }
         }
@@ -103,57 +98,59 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
         mSendBtn.setOnClickListener(this)
 
     }
+
     @SuppressLint("NewApi")
     override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.checkbtn ->{
-                if(!mAddressEdt.text.isEmpty()){
+        when (v!!.id) {
+            R.id.checkbtn -> {
+                if (!mAddressEdt.text.isEmpty()) {
                     checkAddress(mAddressEdt.text.toString())
-                }else{
-                    Toast.makeText(this,"請勿輸入空白",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "請勿輸入空白", Toast.LENGTH_SHORT).show()
                 }
 
             }
-            R.id.startbtn ->{
+            R.id.startbtn -> {
                 clickTimePicker(mStartbtn)
 
             }
-            R.id.endbtn ->{
+            R.id.endbtn -> {
                 clickTimePicker(mEndbtn)
 
             }
-            R.id.button4 ->{
-
+            R.id.button4 -> {
+                selectPic()
             }
-            R.id.send ->{
+            R.id.send -> {
 
             }
         }
     }
-    fun checkAddress(addrss:String){
+
+    fun checkAddress(addrss: String) {
         var geoCoder = Geocoder(this, Locale.getDefault())
         var addressLocation = geoCoder.getFromLocationName(addrss, 1)
-        if (addressLocation.size!=0){
+        if (addressLocation.size != 0) {
             var latitude = addressLocation[0].latitude
             var longitude = addressLocation[0].longitude
-            Log.d("latitude",latitude.toString())
-            Log.d("longitude",longitude.toString())
+            Log.d("latitude", latitude.toString())
+            Log.d("longitude", longitude.toString())
             val builder = AlertDialog.Builder(this)
             builder.setTitle("取得成功")
-            builder.setMessage("latitude:"+latitude.toString()+"\n\n"
-                    + "longitude:"+ longitude.toString()
+            builder.setMessage("latitude:" + latitude.toString() + "\n\n"
+                    + "longitude:" + longitude.toString()
             )
-            builder.setPositiveButton("知道了",{ dialog, whichButton ->
+            builder.setPositiveButton("知道了", { dialog, whichButton ->
                 dialog.dismiss()
             })
             // create dialog and show it
             val dialog = builder.create()
             dialog.show()
-        }else{
+        } else {
             val builder = AlertDialog.Builder(this)
             builder.setTitle("取得失敗")
             builder.setMessage("請重新檢查地址並重新輸入!!")
-            builder.setPositiveButton("知道了",{ dialog, whichButton ->
+            builder.setPositiveButton("知道了", { dialog, whichButton ->
                 dialog.dismiss()
             })
             // create dialog and show it
@@ -163,16 +160,17 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
 
 
     }
+
     @RequiresApi(Build.VERSION_CODES.N)
     fun clickTimePicker(button: Button) {
         val c = Calendar.getInstance()
         val hour = c.get(Calendar.HOUR)
         val minute = c.get(Calendar.MINUTE)
 
-        val tpd = TimePickerDialog(this,TimePickerDialog.OnTimeSetListener(function = { view, h, m ->
+        val tpd = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener(function = { view, h, m ->
             button.setText(h.toString() + " : " + m)
 
-        }),hour,minute,false)
+        }), hour, minute, false)
 
         tpd.show()
     }
@@ -256,6 +254,7 @@ class MannerActivity : AppCompatActivity(),View.OnClickListener {
             //取得照片路徑uri
             val datauri = data.data
             img = encode(datauri)
+            Log.d("img",img)
             val cr = contentResolver
             try {
 

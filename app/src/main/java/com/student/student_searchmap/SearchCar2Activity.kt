d22f79b9e1df8f10c68761076c4ce5e944dba652
Wei Wei <jheng.wei0919@gmail.com>
2019-11-12 11:33:29 +0800

    add class

---
+++ app/src/main/java/com/student/student_searchmap/SearchCar2Activity.kt
@@ -0,0 +1,25 @@
+package com.student.student_searchmap
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SearchCar2Activity : AppCompatActivity() {
+    lateinit var button:Button
+    lateinit var button2 :Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_car)
+        button = findViewById(R.id.button2)
+        button2 = findViewById(R.id.button3)
+        button.setOnClickListener {
+            startActivity(Intent(this,Maps2Activity::class.java))
+        }
+        button2.setOnClickListener {
+            startActivity(Intent(this,BigMannerActivity::class.java))
+
+        }
+
+    }
+}
